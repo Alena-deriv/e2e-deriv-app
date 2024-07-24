de5934f7fcfed9faee432199565da4b2ae33d3b7
Afsaneh Rezaeinejad <101572229+afsaneh-rezaeinejad-deriv@users.noreply.github.com>
2024-07-24 20:18:30 +0800

    Afsaneh/migration success mobile (#493)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- cypress/e2e/smoke/Wallets/walletMigrationSuccess.cy.js
+++ cypress/e2e/smoke/Wallets/walletMigrationSuccess.cy.js
@@ -1,4 +1,4 @@
-function migratetoWallet() {
+function migratetoWallet(size) {
   cy.get('#modal_root div')
     .filter((index, element) => {
       return Cypress.$(element).text().includes('Introducing WalletsUse')
@@ -10,6 +10,7 @@ function migratetoWallet() {
   cy.findByText(/To complete your Wallet setup/).should('be.visible')
   cy.findByRole('button', { name: 'Log out' }).should('be.visible').click()
   cy.c_login({ relogin: true })
+  cy.c_visitResponsive('/', size)
   cy.findByText('Your Wallets are ready!').should('be.visible')
   cy.findAllByText(/Explore the exciting new/).should('be.visible')
   cy.findByRole('button', { name: 'Get started' }).should('be.visible').click()
@@ -33,67 +34,130 @@ function getCurrencyList() {
       return totalCurrenciesCount
     })
 }
-function checkCyrptoWalletExistance() {
-  const currency_code = ['BTC', 'ETH', 'LTC', 'tUSDT', 'eUSDT', 'USDC']
+function checkCyrptoWalletExistance(size) {
+  const currency_code = ['BTC', 'ETH', 'eUSDT', 'LTC', 'tUSDT', 'USDC']
   // loop to make sure it check for all available currency
   currency_code.forEach((currency_code) => {
-    cy.c_switchWalletsAccount(currency_code)
-    cy.findByText('Withdraw').should('exist')
-    cy.get('.wallets-balance__container').should('contain.text', '0.00')
-    cy.get('section')
-      .contains('OptionsPredict the market,')
-      .parent()
-      .findByRole('button')
-      .should('exist')
-    cy.findByText('Withdraw').click()
+    if (`${size}` == `small`) {
+      cy.c_switchWalletsAccountResponsive(currency_code)
+      cy.findByText('Withdraw').should('exist')
+      cy.get('.wallets-card__details-bottom').should('contain.text', '0.00')
+    } else {
+      cy.c_switchWalletsAccount(currency_code)
+      cy.findByText('Withdraw').should('exist')
+      cy.get('.wallets-balance__container').should('contain.text', '0.00')
+      cy.get('section')
+        .contains('OptionsPredict the market,')
+        .parent()
+        .findByRole('button')
+        .should('exist')
+    }
+    cy.findByText('Withdraw').parent().click()
     cy.contains(`No funds in ${currency_code} Wallet`).should('exist')
-    cy.findByTestId('dt_traders_hub_home_button').click()
-    cy.findByText('Deposit').should('exist', { timeout: 3000 }).click()
-    cy.get('canvas').should('be.visible', { timeout: 3000 })
-    cy.findByTestId('dt_traders_hub_home_button').click()
+    if (`${size}` == `small`) {
+      cy.findByText('Deposit')
+        .should('exist', { timeout: 3000 })
+        .parent()
+        .click()
+      cy.get('canvas').should('be.visible', { timeout: 3000 })
+      cy.findByTestId('dt_close_btn').click()
+    } else {
+      cy.findByTestId('dt_traders_hub_home_button').click()
+      cy.findByText('Deposit')
+        .should('exist', { timeout: 3000 })
+        .parent()
+        .click()
+      cy.get('canvas').should('be.visible', { timeout: 3000 })
+      cy.findByTestId('dt_traders_hub_home_button').click()
+    }
   })
+  if (`${size}` == `small`) {
+    cy.get('div.wallets-progress-bar')
+      .find('div.wallets-progress-bar-inactive')
+      .first()
+      .click()
+  }
 }
-function checkFiatWalletExistance() {
-  cy.findByText('Withdraw').click()
+function checkFiatWalletExistance(size) {
+  cy.get('.traders-hub-header').should('not.contain.text', 'Cashier')
+  cy.findByText('Withdraw').parent().click()
   cy.findByText('No funds in USD Wallet').should('be.visible')
-  cy.findByTestId('dt_traders_hub_home_button').click()
-  cy.findByText('Deposit').should('exist', { timeout: 3000 }).click()
-  cy.get('iframe[class=wallets-deposit-fiat__iframe]').should('be.visible')
-  cy.findByTestId('dt_traders_hub_home_button').click()
+  if (`${size}` == `small`) {
+    cy.findByText('Deposit').should('exist', { timeout: 3000 }).parent().click()
+    cy.get('iframe[class=wallets-deposit-fiat__iframe]').should('be.visible')
+    cy.findByTestId('dt_close_btn').click()
+  } else {
+    cy.findByTestId('dt_traders_hub_home_button').click()
+    cy.findByText('Deposit').should('exist', { timeout: 3000 }).parent().click()
+    cy.get('iframe[class=wallets-deposit-fiat__iframe]').should('be.visible')
+    cy.findByTestId('dt_traders_hub_home_button').click()
+  }
 }
-function checkDemoExistance() {
-  cy.c_switchWalletsAccount('USD Demo')
-  cy.findByText('Reset balance').should('be.visible')
-  cy.c_switchWalletsAccount('USD')
+function checkDemoExistance(size) {
+  if (`${size}` == `small`) {
+    cy.c_switchWalletsAccountDemo()
+    cy.findByText('Reset balance').should('be.visible')
+    cy.wait(2000) // balance is updated with a slight delay in UI
+    cy.get('.wallets-card__details-bottom').should(
+      'contain.text',
+      '10,000.00 USD'
+    )
+    cy.findByRole('button', { name: 'Options' }).click({ force: true })
+    cy.get('.wallets-deriv-apps-section').should(
+      'contain.text',
+      '10,000.00 USD'
+    )
+  } else {
+    cy.c_switchWalletsAccount('USD Demo')
+    cy.findByText('Reset balance').should('be.visible')
+    cy.get('.wallets-balance__container').should(
+      'contain.text',
+      '10,000.00 USD'
+    )
+    cy.get('.wallets-deriv-apps-section').should(
+      'contain.text',
+      '10,000.00 USD'
+    )
+  }
 }
 const countyCode = ['aq']
 countyCode.forEach((countyCode) => {
-  describe(`QATEST-154293 Migration Success Without Fiat payment method before migration for ${countyCode} and QATEST-154290 Migration Success With Fiat and Crypto account`, () => {
+  describe(`QATEST-154290 Migration Success With Fiat and Crypto account ${countyCode}`, () => {
     beforeEach(() => {
       cy.c_createCRAccount({ country_code: countyCode })
     })
-
-    it('should be able to migrate Fiat account', () => {
-      cy.c_login()
-      migratetoWallet()
-      checkFiatWalletExistance()
-      checkDemoExistance()
-    })
-    it('should be able to migrate Fiat with Crypto account', () => {
-      cy.c_login()
-      cy.get('.dc-modal-header__close').click()
-      cy.findByTestId('dt_currency-switcher__arrow').click()
-      cy.findByRole('button', { name: 'Add or manage account' }).click()
-      getCurrencyList().then((totalCurrencyCount) => {
-        cy.findByTestId('dt_modal_close_icon').click()
-        for (let acctCount = 0; acctCount < totalCurrencyCount; acctCount++) {
-          createSiblingAcct(acctCount)
-        }
+    const size = ['small', 'desktop']
+    size.forEach((size) => {
+      it('should be able to migrate Fiat account', () => {
+        cy.c_login()
+        const isMobile = size == 'small' ? true : false
+        cy.c_visitResponsive('/', size)
+        migratetoWallet(size)
+        checkFiatWalletExistance(size)
+        checkDemoExistance(size)
       })
-      cy.findByRole('button', { name: "Let's go" }).click()
-      migratetoWallet()
-      checkCyrptoWalletExistance()
-      checkDemoExistance()
+      it('should be able to migrate Fiat with Crypto account', () => {
+        cy.c_login()
+        const isMobile = size == 'small' ? true : false
+        cy.c_visitResponsive('/', size)
+        cy.get('.dc-modal-header__close').click()
+        cy.findByTestId('dt_currency-switcher__arrow').click()
+        cy.findByRole('button', { name: 'Add or manage account' }).click()
+        getCurrencyList().then((totalCurrencyCount) => {
+          if (isMobile) {
+            cy.findByTestId('dt_dc_mobile_dialog_close_btn').click()
+          } else {
+            cy.findByTestId('dt_modal_close_icon').click()
+          }
+          for (let acctCount = 0; acctCount < totalCurrencyCount; acctCount++) {
+            createSiblingAcct(acctCount)
+          }
+        })
+        cy.findByRole('button', { name: "Let's go" }).click()
+        migratetoWallet(size)
+        checkCyrptoWalletExistance(size)
+        checkDemoExistance(size)
+      })
     })
   })
 })
