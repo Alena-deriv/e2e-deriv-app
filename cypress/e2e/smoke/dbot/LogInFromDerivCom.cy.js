e0e1ea98b7df4daddf58ae6bc71e8b20f23e0885
thivya-deriv <82857131+thivya-deriv@users.noreply.github.com>
2024-07-24 12:00:17 +0800

    Webflow changes code update (#511)
    * Webflow changes code update
    
    * Trade now button check

--- cypress/e2e/smoke/dbot/LogInFromDerivCom.cy.js
+++ cypress/e2e/smoke/dbot/LogInFromDerivCom.cy.js
@@ -4,14 +4,13 @@ describe('QATEST-4126: Log in Deriv Bot platform page from deriv.com', () => {
   const botDashboard = new BotDashboard()
   beforeEach(() => {
     cy.c_visitResponsive(`${Cypress.env('derivComProdURL')}/dbot`, 'desktop')
-    cy.findByText('Get into the Deriv Bot experience').should('exist')
+    cy.findByText('Automated bot trading. No coding required.').should('exist')
   })
 
   it('Login from deriv.com and redirect to dbot on app.deriv.com', () => {
     if (Cypress.config().baseUrl == Cypress.env('prodURL')) {
       // added an if here so later can add on for staging check
-      cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible')
-      cy.findByRole('button', { name: 'Log in' }).click({ force: true })
+      cy.findByRole('link', { name: 'Log in' }).click()
       cy.findByLabelText('Email').type(
         Cypress.env('credentials').production.dBot.ID
       )
@@ -19,10 +18,15 @@ describe('QATEST-4126: Log in Deriv Bot platform page from deriv.com', () => {
         Cypress.env('credentials').production.dBot.PSWD
       )
       cy.findByRole('button', { name: 'Log in' }).click()
+      cy.findAllByTestId('dt_balance_text_container').should('have.length', '2')
+      cy.c_openDbotThub()
       botDashboard.botBuilderDash.should('be.visible')
       cy.c_visitResponsive(`${Cypress.env('derivComProdURL')}dbot`, 'desktop')
-      cy.findByText('Go to live demo')
-        .invoke('attr', 'href')
+      cy.get('.tab-for-buttons')
+        .eq(1)
+        .should('contain.text', 'Trade now')
+        .scrollIntoView({ timeout: 5000 })
+        .should('have.attr', 'href')
         .then((href) => {
           cy.request(href).its('status').should('eq', 200)
         })
