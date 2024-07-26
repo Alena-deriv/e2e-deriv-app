e347cceecc53f01faf5725a912ba6f9648d99451
vimalan-deriv <103023906+vimalan-deriv@users.noreply.github.com>
2024-07-26 16:31:03 +0800

    Crypto deposit mobile (#528)
    * deposit_address
    
    * deposit_address
    
    * deposit_address
    
    * update_PR
    
    * update_PR
    
    * Update_review
    
    * Update_review
    
    * Update_review
    
    * Update_review

--- cypress/e2e/wip/Crypto/crypto_deposit_check.cy.js
+++ cypress/e2e/wip/Crypto/crypto_deposit_check.cy.js
@@ -1,83 +1,91 @@
-describe('QATEST-715 - Load crypto deposit page', () => {
-  beforeEach(() => {
-    cy.c_login({ user: 'allcrypto' })
-    cy.c_visitResponsive('/appstore/traders-hub', { size: 'desktop' })
-  })
+const sizes = ['mobile', 'desktop']
+sizes.forEach((size) => {
+  describe(`QATEST-715 - Test Case summary here on ${size}`, () => {
+    beforeEach(() => {
+      cy.c_login({ user: 'allcrypto' })
+    })
+    it(`should be able to check crypto account from Traders Hub on ${size}`, () => {
+      cy.c_visitResponsive('', { size: size })
+      const isMobile = size == 'mobile' ? true : false
+      const cryptocurrencies = [
+        {
+          name: 'Bitcoin',
+        },
+        {
+          name: 'Ethereum',
+        },
+        {
+          name: 'Litecoin',
+        },
+        {
+          name: 'Tether ERC20',
+        },
+        {
+          name: 'USD Coin',
+        },
+        {
+          name: 'Tether TRC20',
+        },
+      ]
+      cryptocurrencies.forEach((crypto) => {
+        cy.c_closeNotificationHeader()
+        cy.c_selectCurrency(crypto)
+        cy.c_checkTotalAssetSummary()
+        cy.c_closeNotificationHeader()
+        cy.findByRole('button', { name: 'Deposit' }).click()
 
-  it('should be able to create crypto account from Traders Hub.', () => {
-    const cryptocurrencies = [
-      'Bitcoin',
-      'Ethereum',
-      'Litecoin',
-      'Tether ERC20',
-      'USD Coin',
-      'Tether TRC20',
-    ]
-    cryptocurrencies.forEach((crypto) => {
-      cy.findByText('Options & Multipliers').should('be.visible')
-      cy.c_closeNotificationHeader()
-      cy.findByTestId('dt_currency-switcher__arrow')
-        .should('be.visible')
-        .click()
-      cy.findByText(crypto).click()
-      cy.findByTestId('dt_currency-switcher__arrow').should('be.visible')
-      cy.c_closeNotificationHeader()
-      cy.findByRole('button', { name: 'Deposit' }).click()
-
-      // To check for QR code and compare with the code generated
-      cy.get("canvas[class*='qrcode']").should('be.visible') // assert that the QR code element exists
-      cy.get("canvas[class*='qrcode']").then(($canvas) => {
-        // Get the image data from the canvas
-        const imageData = $canvas[0]
-          .getContext('2d')
-          .getImageData(0, 0, $canvas[0].width, $canvas[0].height)
-        // Decode the QR code using jsQR
-        const code = Cypress.jsQR(
-          imageData.data,
-          imageData.width,
-          imageData.height
+        cy.findByText('Transaction status').should('be.visible')
+        cy.get('.transactions-crypto-transaction-status-side-note').should(
+          'exist'
         )
-        // Assert that the QR code was successfully decoded and get its value
-        expect(code).to.not.be.null
-        const qrCodeValue = code.data
-        // Log the value of the QR code
-        cy.log('QR Code Value:', qrCodeValue)
-        // Get the value from the element with the specified class
-        cy.get('.deposit-crypto-wallet-address__hash-container')
-          .invoke('text')
-          .then((actionContainerValue) => {
-            // Ensure that both values are trimmed to remove any leading/trailing whitespace
-            const trimmedQRCodeValue = qrCodeValue.trim()
-            const trimmedActionContainerValue = actionContainerValue.trim()
-            // Compare the QR code value with the value from the specified class
-            expect(trimmedQRCodeValue).to.equal(trimmedActionContainerValue)
-          })
-      })
-      cy.get('.deposit-crypto-wallet-address__hash-container').should('exist') // Assert that the container exists
-      cy.get('.deposit-crypto-wallet-address__action-container').click()
-      cy.get('.deposit-crypto-wallet-address__hash-container')
-        .invoke('text')
-        .then((expectedValue) => {
-          cy.get('.deposit-crypto-wallet-address__action-container')
-            .click()
-            .then(() => {
-              // After clicking, retrieve the value from the clipboard
-              cy.window().then((win) => {
-                cy.wrap(win.navigator.clipboard.readText()).then(
-                  (copiedValue) => {
-                    // Assert that the copied value matches the expected value
-                    expect(copiedValue.trim()).to.equal(expectedValue.trim())
-                  }
-                )
-              })
+        cy.get('.side-note').should('exist')
+        cy.findAllByText
+        cy.findByText('To avoid loss of funds:').should('be.visible')
+        cy.get("canvas[class*='qrcode']").should('be.visible')
+        cy.get("canvas[class*='qrcode']").then(($canvas) => {
+          const imageData = $canvas[0]
+            .getContext('2d')
+            .getImageData(0, 0, $canvas[0].width, $canvas[0].height)
+          const code = Cypress.jsQR(
+            imageData.data,
+            imageData.width,
+            imageData.height
+          )
+          expect(code).to.not.be.null
+          const qrCodeValue = code.data
+          cy.log('QR Code Value:', qrCodeValue)
+          cy.get('.deposit-crypto-wallet-address__hash-container')
+            .invoke('text')
+            .then((actionContainerValue) => {
+              const trimmedQRCodeValue = qrCodeValue.trim()
+              const trimmedActionContainerValue = actionContainerValue.trim()
+              expect(trimmedQRCodeValue).to.equal(trimmedActionContainerValue)
             })
         })
-      cy.findByText('Transaction status').should('be.visible')
-      cy.get('.transactions-crypto-transaction-status-side-note').should(
-        'exist'
-      )
-      cy.get('.side-note').should('exist')
-      cy.findByTestId('dt_traders_hub_home_button').click()
+        cy.get('.deposit-crypto-wallet-address__hash-container').should('exist')
+        cy.get('.deposit-crypto-wallet-address__action-container').click()
+        cy.get('.deposit-crypto-wallet-address__hash-container')
+          .invoke('text')
+          .then((expectedValue) => {
+            cy.get('.deposit-crypto-wallet-address__action-container')
+              .click()
+              .then(() => {
+                cy.window().then((win) => {
+                  cy.wrap(win.navigator.clipboard.readText()).then(
+                    (copiedValue) => {
+                      expect(copiedValue.trim()).to.equal(expectedValue.trim())
+                    }
+                  )
+                })
+              })
+          })
+        if (isMobile) {
+          cy.get('#dt_mobile_drawer_toggle').click()
+          cy.findAllByRole('link', { name: "Trader's Hub" }).click()
+        } else {
+          cy.findByTestId('dt_traders_hub_home_button').click()
+        }
+      })
     })
   })
 })
