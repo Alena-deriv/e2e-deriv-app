b0c1e4896877b5d53de6ff87f2d03e969f79a21b
Afsaneh Rezaeinejad <101572229+afsaneh-rezaeinejad-deriv@users.noreply.github.com>
2024-07-26 16:11:27 +0800

    fix wallet migration test based on new changes (#532)
    * update
    
    * update
    
    * unify code

--- cypress/e2e/smoke/Wallets/walletMigrationSuccess.cy.js
+++ cypress/e2e/smoke/Wallets/walletMigrationSuccess.cy.js
@@ -9,8 +9,8 @@ function migratetoWallet(size) {
   cy.findByText("You're almost there!").should('be.visible')
   cy.findByText(/To complete your Wallet setup/).should('be.visible')
   cy.findByRole('button', { name: 'Log out' }).should('be.visible').click()
-  cy.c_login({ relogin: true })
-  cy.c_visitResponsive('/', size)
+  cy.c_login({ relogin: true, app: 'wallets' })
+  cy.c_visitResponsive('/', { size: size })
   cy.findByText('Your Wallets are ready!').should('be.visible')
   cy.findAllByText(/Explore the exciting new/).should('be.visible')
   cy.findByRole('button', { name: 'Get started' }).should('be.visible').click()
@@ -38,7 +38,7 @@ function checkCyrptoWalletExistance(size) {
   const currency_code = ['BTC', 'ETH', 'eUSDT', 'LTC', 'tUSDT', 'USDC']
   // loop to make sure it check for all available currency
   currency_code.forEach((currency_code) => {
-    if (`${size}` == `small`) {
+    if (`${size}` == `mobile`) {
       cy.c_switchWalletsAccountResponsive(currency_code)
       cy.findByText('Withdraw').should('exist')
       cy.get('.wallets-card__details-bottom').should('contain.text', '0.00')
@@ -54,7 +54,7 @@ function checkCyrptoWalletExistance(size) {
     }
     cy.findByText('Withdraw').parent().click()
     cy.contains(`No funds in ${currency_code} Wallet`).should('exist')
-    if (`${size}` == `small`) {
+    if (`${size}` == `mobile`) {
       cy.findByText('Deposit')
         .should('exist', { timeout: 3000 })
         .parent()
@@ -71,7 +71,7 @@ function checkCyrptoWalletExistance(size) {
       cy.findByTestId('dt_traders_hub_home_button').click()
     }
   })
-  if (`${size}` == `small`) {
+  if (`${size}` == `mobile`) {
     cy.get('div.wallets-progress-bar')
       .find('div.wallets-progress-bar-inactive')
       .first()
@@ -82,7 +82,7 @@ function checkFiatWalletExistance(size) {
   cy.get('.traders-hub-header').should('not.contain.text', 'Cashier')
   cy.findByText('Withdraw').parent().click()
   cy.findByText('No funds in USD Wallet').should('be.visible')
-  if (`${size}` == `small`) {
+  if (`${size}` == `mobile`) {
     cy.findByText('Deposit').should('exist', { timeout: 3000 }).parent().click()
     cy.get('iframe[class=wallets-deposit-fiat__iframe]').should('be.visible')
     cy.findByTestId('dt_close_btn').click()
@@ -94,7 +94,7 @@ function checkFiatWalletExistance(size) {
   }
 }
 function checkDemoExistance(size) {
-  if (`${size}` == `small`) {
+  if (`${size}` == `mobile`) {
     cy.c_switchWalletsAccountDemo()
     cy.findByText('Reset balance').should('be.visible')
     cy.wait(2000) // balance is updated with a slight delay in UI
@@ -121,30 +121,28 @@ function checkDemoExistance(size) {
   }
 }
 const countyCode = ['aq']
+const size = ['mobile', 'desktop']
 countyCode.forEach((countyCode) => {
-  describe(`QATEST-154290 Migration Success With Fiat and Crypto account ${countyCode}`, () => {
-    beforeEach(() => {
-      cy.c_createCRAccount({ country_code: countyCode })
-    })
-    const size = ['small', 'desktop']
-    size.forEach((size) => {
-      it('should be able to migrate Fiat account', () => {
-        cy.c_login()
-        const isMobile = size == 'small' ? true : false
-        cy.c_visitResponsive('/', size)
+  size.forEach((size) => {
+    describe(`QATEST-154290 Migration Success With Fiat and Crypto account ${countyCode} in ${size}`, () => {
+      beforeEach(() => {
+        cy.c_createCRAccount({ country_code: countyCode })
+      })
+      it(`should be able to migrate Fiat account ${size}`, () => {
+        cy.c_login({ size: size })
+        cy.c_visitResponsive('/', { size: size })
         migratetoWallet(size)
         checkFiatWalletExistance(size)
         checkDemoExistance(size)
       })
       it('should be able to migrate Fiat with Crypto account', () => {
         cy.c_login()
-        const isMobile = size == 'small' ? true : false
-        cy.c_visitResponsive('/', size)
+        cy.c_visitResponsive('/', { size: size })
         cy.get('.dc-modal-header__close').click()
         cy.findByTestId('dt_currency-switcher__arrow').click()
         cy.findByRole('button', { name: 'Add or manage account' }).click()
         getCurrencyList().then((totalCurrencyCount) => {
-          if (isMobile) {
+          if (`${size}` == `mobile`) {
             cy.findByTestId('dt_dc_mobile_dialog_close_btn').click()
           } else {
             cy.findByTestId('dt_modal_close_icon').click()
